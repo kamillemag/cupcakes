8c5152a3bbcf0fae6f7c22777b6aa823370599bc
Kamille Magdirila <kamillemagdirila@Kamilles-Air.socal.rr.com>
2024-02-21 11:02:03 -0800

    update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = "pugzy1234"
+app.config['SECRET_KEY'] = "secret1234"
 
 connect_db(app)
 
